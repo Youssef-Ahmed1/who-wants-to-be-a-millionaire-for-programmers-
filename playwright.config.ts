8bcf2c308d4a42dd1e56f63300e8e4ea35dead1a
Youssef Ahmed <moropass1212@gmail.com>
2026-07-14 18:50:18 +0300

    added E2E tests in major parts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,37 +2,35 @@ import { defineConfig, devices } from '@playwright/test';
 
 
 export default defineConfig({
- // globalSetup: require.resolve('./global-setup'),
-  testDir: './tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+    globalSetup: require.resolve("./global-setup"),
+    testDir: "./tests",
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: "html",
 
-   timeout: 60000, // 60 seconds
-  expect: { timeout: 10000 },
+    timeout: 60000, // 60 seconds
+    expect: { timeout: 10000 },
 
-  use: {
-    baseURL: 'http://localhost:3000', // Your dev server
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    use: {
+        baseURL: "http://localhost:3000", // Your dev server
+        trace: "on-first-retry",
+        screenshot: "only-on-failure",
     },
-    {
-      name: 'mobile',
-      use: { ...devices['iPhone 12'] }, // For mobile testing
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+        },
+        {
+            name: "mobile",
+            use: { ...devices["iPhone 12"] }, // For mobile testing
+        },
+    ],
+    webServer: {
+        command: "npm run dev",
+        url: "http://localhost:3000",
+        reuseExistingServer: !process.env.CI,
     },
-  ],
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
-
 });
